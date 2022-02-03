1a27c229c78b9e7b3b3ab412a3d23d11c482c910
Maksim Ivlev <brainlucker@gmail.com>
2022-02-03 23:44:09 +0300

    Added numberOfStations

--- src/main/java/ru/netology/smart_home/Radio.java
+++ src/main/java/ru/netology/smart_home/Radio.java
@@ -1,12 +1,14 @@
 package ru.netology.smart_home;
 
 public class Radio {
-    private int currentStation;
     private int currentVolume;
     private int minVolume = 0;
-    private int maxVolume = 10;
+    private int maxVolume = 100;
+
+    private int currentStation;
+    private int numberOfStations = 10;
     private int firstStation = 0;
-    private int lastStation = 9;
+    private int lastStation = firstStation + numberOfStations - 1;
 
     // Переключение радиостанций
 
@@ -44,15 +46,15 @@ public class Radio {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < minVolume) {
-            return;
-        }
-        if (currentVolume > maxVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
+//    public void setCurrentVolume(int currentVolume) {
+//        if (currentVolume < minVolume) {
+//            return;
+//        }
+//        if (currentVolume > maxVolume) {
+//            return;
+//        }
+//        this.currentVolume = currentVolume;
+//    }
 
     public void increaseVolume() {
         if (currentVolume < maxVolume) {
